256f1e6fe24ddf11d6081537a305b65f38946c11
Moritz Bley <Moritz.Bley@rub.com>
2025-04-24 12:53:50 +0200

    Updated keymap for autocompletion

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -25,7 +25,24 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    -- keymap = { preset = 'default' },
+    keymap = {
+      -- set to 'none' to disable the 'default' preset
+      preset = "default",
+
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+    },
+
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
